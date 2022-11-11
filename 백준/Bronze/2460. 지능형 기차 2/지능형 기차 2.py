d09584c0f16951d087d54e78a5bc361853087d13
heavenlyview <101788676+heavenlyview@users.noreply.github.com>
2022-11-11 09:46:53 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -0,0 +1,12 @@
+# 최댓값 함수를 이용하는 코드
+
+A = []
+people_result = 0   # 초기화 먼저 해주고 시작
+for i in range(10) : 
+    Out_people, In_people = map(int, input().split()) # 내린 사람, 탄 사람 순서 대로 입력
+    people_result += In_people   # 탄 사람이 입력되면 people_result 변수값 추가
+    people_result -= Out_people  # 반대로 내린 사람이 입력되면 people_result 변수값 차감
+    
+    A.append(people_result)
+
+print(max(A))  # 최댓값 구할거니까 max함수 이용해서 이 중에 최댓값 구해줘
